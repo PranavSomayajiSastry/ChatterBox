e0958c625915875579f0d823036b89357e43fb1a
PranavSomayajiSastry <58330938+PranavSomayajiSastry@users.noreply.github.com>
2020-06-10 17:43:10 +0530

    Add files via upload
    Chatter Box

---
+++ Main.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Avatar } from "react-native-elements";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+class Main extends React.Component {
+  static navigationOptions = {
+    title: 'Chatter',
+  };
+
+  state = {
+    name: "#",
+    image:"#"
+  };
+  selectPicture = async () => {
+    const { cancelled, uri } = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!cancelled) {
+      this.uploadImage(uri, this.state.userId);
+    }
+  };
+
+  uploadImage = async (uri, imageName) => {
+    var response = await fetch(uri);
+    var blob = await response.blob();
+
+    var ref = firebase
+      .storage()
+      .ref()
+      .child("user_profiles/" + imageName);
+
+    return ref.put(blob).then((response) => {
+      this.fetchImage(imageName);
+    });
+  };
+
+  fetchImage = (imageName) => {
+    var storageRef = firebase
+      .storage()
+      .ref()
+      .child("user_profiles/" + imageName);
+
+    // Get the download URL
+    storageRef
+      .getDownloadURL()
+      .then((url) => {
+        this.setState({ image: url });
+      })
+      .catch((error) => {
+        this.setState({ image: "#" });
+      });
+  };
+
+
+  onPress = () =>
+    this.props.navigation.navigate('Chat', { name: this.state.name });
+
+  onChangeText = name => this.setState({ name });
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.title}>Enter your name:</Text>
+        <TextInput
+          style={styles.nameInput}
+          placeHolder="John Cena"
+          onChangeText={this.onChangeText}
+          value={this.state.name}
+        />
+        <Avatar
+            rounded
+            source={{
+              uri: this.state.image,
+            }}
+            size="medium"
+            onPress={() => this.selectPicture()}
+            containerStyle={styles.imageContainer}
+            showEditButton
+          />
+        <TouchableOpacity onPress={this.onPress}>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const offset = 24;
+const styles = StyleSheet.create({
+  title: {
+    marginTop: offset,
+    marginLeft: offset,
+    fontSize: offset,
+  },
+  nameInput: {
+    height: offset * 2,
+
+    margin: offset,
+    paddingHorizontal: offset,
+    borderColor: '#111111',
+    borderWidth: 1,
+  },
+  buttonText: {
+    marginLeft: offset,
+    fontSize: offset,
+  },
+  imageContainer: {
+    flex: 0.75,
+    width: "40%",
+    height: "20%",
+    marginLeft: 20,
+    marginTop: 30,
+    borderRadius: 40,
+  },
+});
+
+export default Main;
